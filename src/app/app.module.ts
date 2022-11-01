94f58e1315e193fb7823011ec3a09c803da0a8a8
Rishabh Gist <damyrachit@gmail.com>
2022-11-02 00:48:12 +0530

    added header and card component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { CardComponent } from './card/card.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CardComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule
