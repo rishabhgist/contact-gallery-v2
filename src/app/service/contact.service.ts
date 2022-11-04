359584cfe9995cbec82be7b1a1f15769c45c9fe0
sneh-shin <snehashinde800@gmail.com>
2022-11-04 22:25:24 +0530

    imported Observable and added getContacts and addContact functions

--- src/app/service/contact.service.ts
+++ src/app/service/contact.service.ts
@@ -1,10 +1,20 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import {Observable} from 'rxjs';
+import { Contact } from '../model/contact';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ContactService {
   URL:string="http://localhost:3000/contacts"
-  constructor() { }
+
+  constructor(private httpClient:HttpClient) { }
   
+  getContacts():Observable<Array<Contact>>{
+    return this.httpClient.get<Array<Contact>>(this.URL)
+  }
+  addContact(data:Contact){
+    return this.httpClient.post(this.URL,data)
+  }
 }
